b8c2b806edaca33154225f3dd4b3c40f4807c30f
Drew Ogle <drew@aperturedata.io>
2022-05-06 15:31:32 -0400

    add loading
    - save state in sql database in case of processing errors
    - create descriptor set
    - add image,bounding and descriptor with associated blobs.

--- tools/using_fr.py
+++ tools/using_fr.py
@@ -13,10 +13,42 @@ import face_recognition
 
 import glob
 
-def get_descriptors(imagePath , outputPath,blob_array):
+from aperturedb import Utils, Connector
 
+from sqlitedict import SqliteDict
+
+mydict = SqliteDict('./processed.sqlite', autocommit=True)
+for key, value in mydict.items():
+    print(key, value)
+print(len(mydict))
+
+
+
+
+user = "admin"
+password = "admin"
+
+con = Connector.Connector(user=user, password=password, port=55557)
+#utils = Utils.Utils(con)
+#utils.remove_descriptorset(search_set_name)
+#utils.add_descriptorset(search_set_name, 512, metric="L2", engine="FaissFlat")
+
+errors = 0
+
+search_set_name = "celebs"
+
+def get_descriptors(name,imagePath , outputPath,blob_array):
+
+    input_name = basename( imagePath )
+    number = input_name[:-4]
+    processed_key = f"{name}__{number}" 
+    if processed_key in mydict:
+        return
     print(f"Reading {imagePath}")
     image = cv2.imread(imagePath)
+    fd = open(imagePath, 'rb')
+    rawImage = fd.read()
+    fd.close()
     try:
         print(f"{image.shape}")
         x_scale = 1.0
@@ -27,8 +59,8 @@ def get_descriptors(imagePath , outputPath,blob_array):
                 x_scale = y_scale = 0.6
             else:
                 x_scale = y_scale = 0.8
-        #if x_scale < 1.0 or y_scale < 1.0:
-        #    image = cv2.resize(image, (0,0), fx=x_scale,fy=y_scale)
+        if x_scale < 1.0 or y_scale < 1.0:
+            image = cv2.resize(image, (0,0), fx=x_scale,fy=y_scale)
     except Exception:
         if image is None:
             print(f"failed to load {imagePath} ( no image )")
@@ -51,7 +83,6 @@ def get_descriptors(imagePath , outputPath,blob_array):
         cv2.putText(image, str(counter), (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
         counter += 1
 
-    input_name = basename( imagePath )
     output_path =join( outputPath,f"{input_name[:-4]}.jpg") 
     print(f"Outputting to {output_path}")
     cv2.imwrite(output_path, image)
@@ -62,16 +93,86 @@ def get_descriptors(imagePath , outputPath,blob_array):
     # print (len(encodings[0]))
     # print (encodings[0])
 
+    #ignore image if no boxes.
+    if len(boxes) == 0 :
+       return
+    (top,right,bottom,left) = boxes[0]
+    t = [
+            {
+                "AddImage": {
+                    "_ref": 1,
+                    "properties": {
+                        "celeb": name,
+                        "celeb_id": "{}_{}".format(name.replace(' ','_'),number),
+                        "class" : "celeb"
+                        },
+                    }
+                }, {
+                    "AddBoundingBox": {
+                        "_ref": 2,
+                        "image": 1,
+                        "rectangle": {
+                            "x": top,
+                            "y": left,
+                            "width": right - left,
+                            "height": bottom - top
+                            }
+                        }
+                    }, {
+                        "AddDescriptor": {
+                            "set": search_set_name,
+                            "connect": {
+                                "ref": 1
+                                }
+                            }
+                        }
 
-    for ele in encodings:
-        blob_array.append(np.array(ele).astype('float32').tobytes())
+                    ]
+
+    float_array = np.array(encodings[0]).astype('float32')
+    array_len = len(float_array)
+    #print("Encodings: {}".format(len(encodings)))
+    print("Array: {}".format(len(float_array)))
+    keypoints = f"{array_len}"
+    for encoding_float in float_array:
+        keypoints = f"{keypoints} {encoding_float}"
+
+    #print(f"{keypoints}")
+
+#   # t[0]["AddImage"]["properties"]["keypoints"] = f"10 {p['lefteye_x']} {p['lefteye_y']} {p['righteye_x']} {p['righteye_y']} {p['nose_x']} {p['nose_y']} {p['leftmouth_x']} {p['leftmouth_y']} {p['rightmouth_x']} {p['rightmouth_y']}"
+    #t[0]["AddImage"]["properties"]["keypoints"] = keypoints
+    (res,blo) = con.query(t,[rawImage,float_array.tobytes()])
+    if isinstance(res,dict) and res['status'] < 0:
+        print("{}".format(res))
+        sys.exit(0)
+    print("{}".format(res))
+    mydict[processed_key] = True
+
+
 
     #return blob_array
-
+TEST_SINGLE=False
 if __name__ == "__main__":
   base = sys.argv[1]
   outbase = "feature_vectors"
   print(f"processing from {base} using dir_??? glob")
+  if not "_descriptor_set" in mydict:
+    query = [{
+            "AddDescriptorSet": {
+                "name":       search_set_name,
+                "dimensions": 128
+            }
+        }]
+    (res,blo) = con.query(query)
+    print("{}".format(res))
+    if "status" in res and res['status'] < 0:
+        print("{}".format(res))
+        sys.exit(0)
+    if "status" in res[0]["AddDescriptorSet"] and res[0]["AddDescriptorSet"]["status"] < 0:
+        print("{}".format(res))
+        sys.exit(0)
+    mydict["_descriptor_set"] = True
+
   blob_array = []
   for globbed in glob.glob( join( base , "dir_???")):
       print(f"* {globbed}")
@@ -83,11 +184,16 @@ if __name__ == "__main__":
             if not exists( out_celeb_dir ):
                 os.mkdir(out_celeb_dir)
             celeb_image_dir = join( globbed, celeb_dir )
-            for image in os.listdir( celeb_image_dir ):
-                print(f" ** {image}")
+            for image_number in range(1,20): #os.listdir( celeb_image_dir ):
+                image = f"{image_number}.jpg"
                 image_path = join( celeb_image_dir, image )
-                get_descriptors(image_path,out_celeb_dir,blob_array)
-            sys.exit(0)
-  fp = open( join(outbase,"features.npy"),'wb')
-  fp.write(blob_array)
-  fp.close()
+                if not exists( image_path ):
+                    continue
+                print(f" ** {image}")
+                get_descriptors(celeb_dir,image_path,out_celeb_dir,blob_array)
+            if TEST_SINGLE:
+                sys.exit(0)
+  #fp = open( join(outbase,"features.npy"),'wb')
+  #fp.write(blob_array)
+  #fp.close()
+  mydict.close()
